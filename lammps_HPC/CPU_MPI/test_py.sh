5d8992dcd4827f9bd1156d696d6a54106e5d714c
Ethan Edmunds <eledmunds1@sheffield.ac.uk>
2025-08-19 12:06:59 +0100

    Updating CPU version

--- lammps_HPC/CPU_MPI/test_py.sh
+++ lammps_HPC/CPU_MPI/test_py.sh
@@ -3,8 +3,8 @@
 #SBATCH --mail-user=eledmunds1@sheffield.ac.uk
 #SBATCH --time=00:10:00
 #SBATCH --mem-per-cpu=1GB
-#SBATCH --cpus-per-task=20
-#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=4
 
 # Load the host's MPI module
 module load OpenMPI/4.1.4-GCC-12.2.0
@@ -13,13 +13,13 @@ module load OpenMPI/4.1.4-GCC-12.2.0
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 # Define the path to your Apptainer container
-CONTAINER_PATH=lmpcpu.sif
+CONTAINER_PATH=lmp_cpu.sif
 
 # Define the path to the host's MPI libraries
 HOST_MPI_PATH=/opt/apps/testapps/el7/software/staging/OpenMPI/4.1.4-GCC-12.2.0
 
 # Run the container using srun, binding the host's MPI libraries
-mpirun -np $ntasks apptainer run --bind $HOST_MPI_PATH:$HOST_MPI_PATH $CONTAINER_PATH -in ../00_examples/example.lmp
+mpirun -np $SLURM_NTASKS apptainer run --bind $HOST_MPI_PATH:$HOST_MPI_PATH $CONTAINER_PATH python ../00_examples/example_1.py
 
 
 
