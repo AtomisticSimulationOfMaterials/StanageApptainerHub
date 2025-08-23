46ed45a92d2c2542860e9e4842494295120b67d5
Ethan Edmunds <eledmunds1@sheffield.ac.uk>
2025-08-23 10:16:41 +0100

    Update to use multiple cpus

--- lammps_HPC/GPU_KK/test_lmp.sh
+++ lammps_HPC/GPU_KK/test_lmp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=TEST_LAMMPS_CPU
+#SBATCH --job-name=TEST_LAMMPS_KK
 #SBATCH --mail-user=---@sheffield.ac.uk
 #SBATCH --time=00:10:00
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
 
 # Paths
 CONTAINER_PATH=lmp_GPU_KK.sif
