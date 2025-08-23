1ef3816b405bec7741f5403b91c8eaf99ed9a9f0
Ethan Edmunds <eledmunds1@sheffield.ac.uk>
2025-08-23 09:08:23 +0100

    Experimenting with calling commands

--- lammps_HPC/GPU_KK/test_lmp.sh
+++ lammps_HPC/GPU_KK/test_lmp.sh
@@ -5,6 +5,7 @@
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:2
+#SBATCH --ntasks=2
 
 module load OpenMPI/4.1.4-GCC-12.2.0
 
@@ -16,8 +17,8 @@ CONTAINER_PATH=lmp_GPU_KK.sif
 HOST_MPI_PATH=/opt/apps/testapps/el7/software/staging/OpenMPI/4.1.4-GCC-12.2.0
 
 # Run LAMMPS inside the Apptainer container
-srun apptainer exec \
-     --bind $HOST_MPI_PATH:$HOST_MPI_PATH \
+apptainer exec \
      $CONTAINER_PATH \
-     lmp -k on g 2 -sf kk \
+     mpiexec.openmpi -np $SLURM_NTASKS \
+     lmp -k on g $SLURM_GRES -sf kk \
      -in ../00_examples/example_2.lmp
