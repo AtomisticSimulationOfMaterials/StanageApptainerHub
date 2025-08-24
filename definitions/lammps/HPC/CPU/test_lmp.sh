09fe00a5d2c7de4eec6e50b946b0f2a112e2372a
Ethan Edmunds <eledmunds1@sheffield.ac.uk>
2025-08-24 13:45:23 +0100

    Updating shell script

--- definitions/lammps/HPC/CPU/test_lmp.sh
+++ definitions/lammps/HPC/CPU/test_lmp.sh
@@ -9,10 +9,9 @@
 module load OpenMPI/4.1.4-GCC-12.2.0
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-CONTAINER_PATH="$SCRIPT_DIR/lmp_CPU.sif"
-INPUT="$SCRIPT_DIR/../../../../examples/lammps/scripts/example_1.lmp"
 HOST_MPI_PATH=/opt/apps/testapps/el7/software/staging/OpenMPI/4.1.4-GCC-12.2.0
+
+INPUT=../examples/scripts/example_1.lmp
 
 srun apptainer exec \
      --bind $HOST_MPI_PATH:$HOST_MPI_PATH \
