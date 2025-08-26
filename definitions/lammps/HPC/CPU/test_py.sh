43ab899eb2e55476a1d2cbb75e46cb43424442ad
Ethan Edmunds <eledmunds1@sheffield.ac.uk>
2025-08-26 14:02:37 +0100

    Updating to use HOST MPI

--- definitions/lammps/HPC/CPU/test_py.sh
+++ definitions/lammps/HPC/CPU/test_py.sh
@@ -6,10 +6,16 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=4
 
+module load OpenMPI/4.1.4-GCC-12.2.0
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+HOST_MPI_PATH=/opt/apps/testapps/el7/software/staging/OpenMPI/4.1.4-GCC-12.2.0
+
 CONTAINER_PATH=lmp_CPU.sif
 
 INPUT=../00_scripts/example_1.py
 
 srun apptainer exec \
+     --bind $HOST_MPI_PATH:$HOST_MPI_PATH \
      $CONTAINER_PATH \
      python $INPUT
