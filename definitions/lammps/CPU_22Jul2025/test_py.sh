eb54bd004044b5b13b6bed6afdf294d2ea0bed01
Ethan Edmunds <eledmunds1@sheffield.ac.uk>
2025-08-27 11:21:50 +0100

    More cores for tests

--- definitions/lammps/CPU_22Jul2025/test_py.sh
+++ definitions/lammps/CPU_22Jul2025/test_py.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=00:10:00
 #SBATCH --mem-per-cpu=500MB
 #SBATCH --cpus-per-task=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=20
 
 module load OpenMPI/4.1.4-GCC-12.2.0
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
